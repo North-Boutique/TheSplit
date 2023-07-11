78e0bbe23ca1604d17cf66eeaf9a52c066d602a7
Anton <antonmihail@gmail.com>
2023-07-10 21:54:44 -0400

    updating forms

--- src/components/CreateNew/GenerateSplit.tsx
+++ src/components/CreateNew/GenerateSplit.tsx
@@ -1,10 +1,27 @@
-import {Box, Text} from 'native-base';
-import React from 'react';
+import {Box, Slider, Text} from 'native-base';
+import React, {useState} from 'react';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function GenerateSplit() {
+  const [onChangeEndValue, setOnChangeEndValue] = useState<number>(0);
   return (
     <Box>
-      <Text>GenerateSplit</Text>
+      <Text>Generate Split</Text>
+      <Box alignItems="center" w="100%">
+        <Text textAlign="center">{onChangeEndValue} Excersies</Text>
+        <Slider
+          minValue={5}
+          maxValue={15}
+          colorScheme="indigo"
+          onChangeEnd={v => {
+            v && setOnChangeEndValue(Math.floor(v));
+          }}>
+          <Slider.Track>
+            <Slider.FilledTrack />
+          </Slider.Track>
+          <Slider.Thumb />
+        </Slider>
+      </Box>
     </Box>
   );
 }
--- src/components/CreateNew/GenerateWorkout.tsx
+++ src/components/CreateNew/GenerateWorkout.tsx
@@ -1,10 +1,83 @@
-import {Box, Text} from 'native-base';
-import React from 'react';
+import {Box, Checkbox, CheckIcon, Select, Slider, Text} from 'native-base';
+import React, {useCallback, useEffect, useState} from 'react';
+import {MuscleGroupNames} from '../../services/types';
 
 function GenerateWorkout() {
+  const [numberOfWorkouts, setNumberOfWorkouts] = useState<number>(0);
+  const [muscleGroup, setMuscleGroup] = useState<MuscleGroupNames[]>();
+  const [maxSelected, setMaxSelected] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (muscleGroup) {
+      if (muscleGroup?.length === 2) {
+        setMaxSelected(true);
+      } else if (muscleGroup?.length < 2 && maxSelected) {
+        setMaxSelected(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [muscleGroup]);
+
+  const isDisabled = useCallback(
+    (name: MuscleGroupNames) => {
+      return !muscleGroup?.indexOf(name) && maxSelected;
+    },
+    [maxSelected, muscleGroup],
+  );
+
   return (
     <Box>
-      <Text>GenerateWorkout</Text>
+      <Text>Generate Workout</Text>
+      <Box alignItems="center" w="100%">
+        <Text textAlign="center">{numberOfWorkouts} Excersies</Text>
+        <Slider
+          minValue={5}
+          maxValue={15}
+          colorScheme="indigo"
+          onChangeEnd={v => {
+            v && setNumberOfWorkouts(Math.floor(v));
+          }}>
+          <Slider.Track>
+            <Slider.FilledTrack />
+          </Slider.Track>
+          <Slider.Thumb />
+        </Slider>
+        <Text textAlign="center">Muscle Group(s)</Text>
+        <Checkbox.Group
+          onChange={setMuscleGroup}
+          value={muscleGroup}
+          accessibilityLabel="choose muscle group"
+          display="inline-flex"
+          justifyContent={'space-evenly'}>
+          <Checkbox isDisabled={isDisabled('Quads')} value="Quads">
+            Quads
+          </Checkbox>
+          <Checkbox isDisabled={isDisabled('Quads')} value="Biceps">
+            Biceps
+          </Checkbox>
+          <Checkbox isDisabled={isDisabled('Shoulders')} value="Shoulders">
+            Shoulders
+          </Checkbox>
+          <Checkbox isDisabled={isDisabled('Chest')} value="Chest">
+            Chest
+          </Checkbox>
+          <Checkbox isDisabled={isDisabled('Triceps')} value="Triceps">
+            Triceps
+          </Checkbox>
+          <Checkbox isDisabled={isDisabled('Back')} value="Back">
+            Back
+          </Checkbox>
+          <Checkbox isDisabled={isDisabled('Glutes')} value="Glutes">
+            Glutes
+          </Checkbox>
+          <Checkbox isDisabled={isDisabled('Hamstrings')} value="Hamstrings">
+            Hamstrings
+          </Checkbox>
+          <Checkbox isDisabled={isDisabled('Abs')} value="Abs">
+            Abs
+          </Checkbox>
+        </Checkbox.Group>
+      </Box>
     </Box>
   );
 }
